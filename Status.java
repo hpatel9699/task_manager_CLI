20b84acb65f40dc695f63d08d1a33acd4163e184
Hetvi Patel <60538305+hpatel9699@users.noreply.github.com>
2025-02-17 20:57:45 -0500

    added the declarations

---
+++ Status.java
@@ -0,0 +1,26 @@
+public enum Status {
+
+    //status in to-do
+    //In-progress
+    //Done
+
+        TODO("Todo"),
+        IN_PROGRESS("In progress"),
+        DONE("Done");
+
+        private final String value;
+
+        Status(String value){
+            this.value = value;
+        }
+
+        public String getValue() {
+            return value;
+        }
+
+        @Override
+        public String toString() {
+            return value;
+        }
+    }
+
---
+++ task.java
@@ -0,0 +1,18 @@
+import java.time.LocalDateTime;
+
+public class task{
+    //id: A unique identifier for the task
+    //description: A short description of the task
+    //status: The status of the task (todo, in-progress, done)
+    //createdAt: The date and time when the task was created
+    //updatedAt: The date and time when the task was last updated
+
+    private int id;
+    private String description;
+    private Status status;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+
+
+}
